8dd678c44378564029f4d823345491ba2df2b3ee
Aman Sinha <sinhaman1909@gmail.com>
2022-04-26 19:36:07 +0530

    Added About component

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+    This is an about component
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, hic maxime minus aperiam iure illo! Aliquid suscipit dolore, natus voluptas dicta, optio est tenetur nemo maiores, ullam nihil recusandae tempora?</p>
+    </div>
+  )
+}
